23b0ba83dac8b5e464339e0a1797b5947d52a6ae
Luiz166 <otavio.tata.morais@gmail.com>
2022-09-11 00:04:01 -0300

    começado a criar a landing page

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/parser.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [Color(0xff5430F4), Color(0xffB4A4FA)])),
+        child: Column(children: [
+          SvgPicture.asset(
+            "assets/images/logo.svg",
+            width: 200,
+          )
+        ]),
+      ),
+    );
   }
 }
