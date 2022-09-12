809d406e62c1cc76f69afc6da7344431c1823c85
Luiz166 <otavio.tata.morais@gmail.com>
2022-09-12 02:25:51 -0300

    retirado a marca d'água de debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:etec_ds/screens/welcome_register.dart';
 import 'package:etec_ds/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
